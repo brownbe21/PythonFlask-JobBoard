ffd2d7037e595db2867a83a5c8015d7f39e0996a
Bennie <brownbe21@gmail.com>
2018-12-31 21:13:48 -0500

    Completed the module

--- jobs/app.py
+++ jobs/app.py
@@ -12,7 +12,7 @@ def open_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
-def execute_sql(sql, values(), commit=False, single=False):
+def execute_sql(sql, values=(), commit=False, single=False):
     connection = open_connection()
     cursor = connection.execute(sql, values)
     if commit == True:
@@ -22,7 +22,7 @@ def execute_sql(sql, values(), commit=False, single=False):
     cursor.close()
     return results
 
-@app.teardown_appcontext            
+@app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
     if connection is not None:
